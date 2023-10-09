751074fa0a4fcd15187c64827300d22ebab0139f
Melli <mailto:nspershina@edu.hse.ru>
2023-10-09 18:25:56 +0300

    Initial Commit

---
+++ OS_Alfa_Second/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OS_Alfa_Second
+//
+//  Created by Нелли Першина on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
