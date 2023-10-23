013a58a74ae2023998cd2bd25f477832f24aff78
Melli795 <nelli.per@icloud.com>
2023-10-23 18:42:33 +0300

    added LichessAPI

--- OS_Alfa_Second/ViewController.swift
+++ OS_Alfa_Second/ViewController.swift
@@ -12,6 +12,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = . systemBlue
-    }
-}
+        let url: URL = URL(string:"https://lichess.org/api")!
+                URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+                    guard
+                        let data = data,
+                            error == nil
+                    else{
+                        return
+                    }
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    let model = try! decoder.decode([LichessDTO].self, from: data)
+                    print(model)
+                }).resume()
+            }
+
+
+        }
+
+        struct LichessDTO: Decodable{
+            let id: Int
+            let name: String
+            let tagline: String
+            let imageUrl: URL
+        }
+
+
 
